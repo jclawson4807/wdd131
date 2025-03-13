f9bfc1fa91822ba6b479a01393a77488aad91ea3
jclawson4807 <jclawsonx@icloud.com>
2025-03-12 19:05:31 -0500

    use defer statement

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,35 +1,32 @@
+const ul = document.querySelector("#list");
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
 
-window.onload = function () {
-    const ul = document.querySelector("#list");
-    const input = document.querySelector("#favchap");
-    const button = document.querySelector("button");
+button.addEventListener("click", function () {
+    const value = input.value.trim();
 
-    button.addEventListener("click", function () {
-        const value = input.value.trim();
-
-        if (value == "") {
-            input.focus();
-        }
-        else {
-            const newLi = document.createElement('li');
-            const newDeleteButton = document.createElement('button');
-
-            newLi.textContent = input.value;
-            newDeleteButton.textContent = '❌';
-            newDeleteButton.ariaLabel = "Delete " + input.value;
-
-            newLi.appendChild(newDeleteButton);
-
-            ul.append(newLi);
-        }
-
-        input.value = "";
-    });
-
-    ul.addEventListener("click", function (event) {
-        const parentLi = event.target.closest("li");
-
-        parentLi.remove();
+    if (value == "") {
         input.focus();
-    });
-}
+    }
+    else {
+        const newLi = document.createElement('li');
+        const newDeleteButton = document.createElement('button');
+
+        newLi.textContent = input.value;
+        newDeleteButton.textContent = '❌';
+        newDeleteButton.ariaLabel = "Delete " + input.value;
+
+        newLi.appendChild(newDeleteButton);
+
+        ul.append(newLi);
+    }
+
+    input.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+    const parentLi = event.target.closest("li");
+
+    parentLi.remove();
+    input.focus();
+});
