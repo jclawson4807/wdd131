a32cc55f189434bee71e4f930bb1fb0618e45cfb
jclawson4807 <jclawsonx@icloud.com>
2025-04-06 19:26:56 -0500

    home and article filter work

--- scripts/project-online-sources.js
+++ scripts/project-online-sources.js
@@ -6,11 +6,36 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 });
 
+const homeButton = document.querySelector('#home');
+const familyinfoButton = document.querySelector('#familyinfo');
+const onlinesourcesButton = document.querySelector('#onlinesources');
+const researchhelpsButton = document.querySelector('#researchhelps');
+
+homeButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "none");
+    window.location.replace("project.html");
+});
+
+familyinfoButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "familyinfo");
+    window.location.replace("project-online-sources.html");
+});
+
+onlinesourcesButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "onlinesources");
+    window.location.replace("project-online-sources.html");
+});
+
+researchhelpsButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "researchhelps");
+    window.location.replace("project-online-sources.html");
+});
+
 const main = document.querySelector("main");
 
 const articles = [
     {
-        articleType: "OnlineSources",
+        articleType: "onlinesources",
         articleTitle: "Family Search",
         authorName: "James Clawson",
         authorImage: "author-image.webp",
@@ -68,7 +93,9 @@ function generateArticleSection(article) {
     return returnString
 }
 
-const newArticles = articles.filter((article) => article.articleType == "OnlineSources");
+let filter = window.localStorage.getItem("articleFilter");
+
+const newArticles = articles.filter((article) => article.articleType == filter);
 
 newArticles.forEach(article => {
     let returnString = generateArticleSection(article);
--- scripts/project.js
+++ scripts/project.js
@@ -6,4 +6,28 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 });
 
+const homeButton = document.querySelector('#home');
+const familyinfoButton = document.querySelector('#familyinfo');
+const onlinesourcesButton = document.querySelector('#onlinesources');
+const researchhelpsButton = document.querySelector('#researchhelps');
+
+homeButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "none");
+    window.location.replace("project.html");
+});
+
+familyinfoButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "familyinfo");
+    window.location.replace("project-online-sources.html");
+});
+
+onlinesourcesButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "onlinesources");
+    window.location.replace("project-online-sources.html");
+});
+
+researchhelpsButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "researchhelps");
+    window.location.replace("project-online-sources.html");
+});
 
