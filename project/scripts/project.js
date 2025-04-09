4c94fed00e3e6d0f33055a6ad49965c837f66d70
jclawson4807 <jclawsonx@icloud.com>
2025-04-08 20:36:56 -0500

    made common question boxes clickable

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -11,6 +11,10 @@ const familyinfoButton = document.querySelector('#familyinfo');
 const onlinesourcesButton = document.querySelector('#onlinesources');
 const researchhelpsButton = document.querySelector('#researchhelps');
 
+const familyinfoP = document.querySelector('#familyinfo-p');
+const onlinesourcesP = document.querySelector('#onlinesources-p');
+const researchhelpsP = document.querySelector('#researchhelps-p');
+
 homeButton.addEventListener('click', () => {
     localStorage.setItem("articleFilter", "none");
 });
@@ -26,6 +30,21 @@ onlinesourcesButton.addEventListener('click', () => {
 });
 
 researchhelpsButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "researchhelps");
+    window.location.replace("project-articles.html");
+});
+
+familyinfoP.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "familyinfo");
+    window.location.replace("project-articles.html");
+});
+
+onlinesourcesP.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "onlinesources");
+    window.location.replace("project-articles.html");
+});
+
+researchhelpsP.addEventListener('click', () => {
     localStorage.setItem("articleFilter", "researchhelps");
     window.location.replace("project-articles.html");
 });
