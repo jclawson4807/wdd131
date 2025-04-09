c53c87ff24016b166ea47bd6b9bead98d01f34fc
jclawson4807 <jclawsonx@icloud.com>
2025-04-08 20:31:25 -0500

    added referenced materials page and link to footer

---
+++ project/scripts/referenced-materials.js
@@ -0,0 +1,50 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const homeButton = document.querySelector('#home');
+const familyinfoButton = document.querySelector('#familyinfo');
+const onlinesourcesButton = document.querySelector('#onlinesources');
+const researchhelpsButton = document.querySelector('#researchhelps');
+
+const pageIdentifierName = document.querySelector("#page-identifier-name");
+
+const main = document.querySelector("main");
+
+homeButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "none");
+    window.location.replace("project.html");
+});
+
+familyinfoButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "familyinfo");
+    window.location.replace("project-articles.html");
+});
+
+onlinesourcesButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "onlinesources");
+    window.location.replace("project-articles.html");
+});
+
+researchhelpsButton.addEventListener('click', () => {
+    localStorage.setItem("articleFilter", "researchhelps");
+    window.location.replace("project-articles.html");
+});
+
+if (window.localStorage.getItem("emailAddress") === null) {
+    window.location.replace("project.html");
+}
+else {
+    let storedEmailAddress = window.localStorage.getItem("emailAddress");
+
+    const emailAddressSpan = document.querySelector("#email-address");
+    emailAddressSpan.textContent = storedEmailAddress;
+
+};
+
+
+
