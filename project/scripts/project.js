ab6ad22185e23c800cbb6be5040decfb852c178f
jclawson4807 <jclawsonx@icloud.com>
2025-04-14 11:57:00 -0500

    integrating basic navigation

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,44 +1,6 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
-
-const homeButton = document.querySelector('#home');
-const familyinfoButton = document.querySelector('#familyinfo');
-const onlinesourcesButton = document.querySelector('#onlinesources');
-const toolsButton = document.querySelector('#tools');
-const researchhelpButton = document.querySelector('#researchhelp');
-
 const familyinfoP = document.querySelector('#familyinfo-p');
 const onlinesourcesP = document.querySelector('#onlinesources-p');
 const toolsP = document.querySelector('#tools-p');
-
-homeButton.addEventListener('click', () => {
-    localStorage.setItem("articleFilter", "none");
-});
-
-familyinfoButton.addEventListener('click', () => {
-    localStorage.setItem("articleFilter", "familyinfo");
-    window.location.replace("project-articles.html");
-});
-
-onlinesourcesButton.addEventListener('click', () => {
-    localStorage.setItem("articleFilter", "onlinesources");
-    window.location.replace("project-articles.html");
-});
-
-toolsButton.addEventListener('click', () => {
-    localStorage.setItem("articleFilter", "tools");
-    window.location.replace("project-articles.html");
-});
-
-researchhelpButton.addEventListener('click', () => {
-    localStorage.setItem("articleFilter", "none");
-    window.location.replace("request-research-help.html");
-});
 
 familyinfoP.addEventListener('click', () => {
     localStorage.setItem("articleFilter", "familyinfo");
